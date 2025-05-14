cf164ab98a5eaadec6e7e71d14b61bd5949bab31
KunalRauniyar05 <kunalrauniyar05@gmail.com>
2025-05-14 16:50:40 +0545

    HTTP METHOD how it works

--- src/app.js
+++ src/app.js
@@ -2,14 +2,33 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello",(req, res) => {
-    res.send("route");
-    
-});
+// it will only make call to get method
+app.get("/user", (req,res) => {
+    res.send({firstname: "Rahul", lastname: "Rauniyar"})
+})
+
+
+app.post("/user",(req,res) => {
+    res.send("Post is tested")
+})
+
+
+app.delete("/user",(req,res) => {
+    res.send("delete is tested")
+})
+
+
+app.patch("/user",(req,res) => {
+    res.send("Patch is tested")
+})
+
+
+// It will matches all the HTTP API calls
 
 app.use("/test",(req,res) => {
     res.send("Hello world")
 })
+
 
 app.listen(3000, () => {
     console.log("Port running on 3000");
