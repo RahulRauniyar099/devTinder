b239e2bcfaad413ee6010a8fac095fe871182272
KunalRauniyar05 <kunalrauniyar05@gmail.com>
2025-05-14 17:19:48 +0545

    Routes optional regex playing

--- src/app.js
+++ src/app.js
@@ -2,32 +2,41 @@ const express = require("express");
 
 const app = express();
 
+
+//optional 
+app.get(/^\/ab?c$/, (req, res) => {
+    console.log("knjbj");
+    
+    res.send("Matched either /ac or /abc");
+});
+
+
 // it will only make call to get method
-app.get("/user", (req,res) => {
-    res.send({firstname: "Rahul", lastname: "Rauniyar"})
-})
+// app.get("/user", (req,res) => {
+//     res.send({firstname: "Rahul", lastname: "Rauniyar"})
+// })
 
 
-app.post("/user",(req,res) => {
-    res.send("Post is tested")
-})
+// app.post("/user",(req,res) => {
+//     res.send("Post is tested")
+// })
 
 
-app.delete("/user",(req,res) => {
-    res.send("delete is tested")
-})
+// app.delete("/user",(req,res) => {
+//     res.send("delete is tested")
+// })
 
 
-app.patch("/user",(req,res) => {
-    res.send("Patch is tested")
-})
+// app.patch("/user",(req,res) => {
+//     res.send("Patch is tested")
+// })
 
 
 // It will matches all the HTTP API calls
 
-app.use("/test",(req,res) => {
-    res.send("Hello world")
-})
+// app.use("/test",(req,res) => {
+//     res.send("Hello world")
+// })
 
 
 app.listen(3000, () => {
