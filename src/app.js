309c066244521abb4270686aeda7afac15df8700
KunalRauniyar05 <kunalrauniyar05@gmail.com>
2025-05-15 15:35:49 +0545

    Database connected and data pushed in mongodb compass

--- src/app.js
+++ src/app.js
@@ -1,19 +1,39 @@
 const express = require("express");
- const connectDB = require("./config/database")
+const connectDB = require("./config/database")
+const User = require("./model/user")
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+
+    //creating new user or instance of Usermodel with userobj
+    const user = new User({
+        firstName: " virat",
+        lastName: "Rauniyar",
+        Email: "virat@gmail.com",
+        password: "virat",
+    })
+
+    //it save in database and returns promise
+    try{
+   await user.save()
+   res.send("User added successfully")
+    } catch(err){
+        res.status(400).send("Data not added", err.message)
+    }
+})
 
 connectDB().then(() => {
 
     console.log("Database conncetion is successfull");
 
     app.listen(3000, () => {
-    console.log("Port running on 3000");
-    
-});
-    
-}).catch(err =>{
-console.error("CONNECTION NOT ESTABLISHED");
+        console.log("Port running on 3000");
+
+    });
+
+}).catch(err => {
+    console.error("CONNECTION NOT ESTABLISHED");
 
 })
 
--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose")
 
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://raulrauniyar09:grSzI0gqxovs5h7I@namastenode.xyvctc4.mongodb.net/?retryWrites=true&w=majority&appName=namasteNode/devTinder")
+    await mongoose.connect("mongodb+srv://raulrauniyar09:grSzI0gqxovs5h7I@namastenode.xyvctc4.mongodb.net/devTinderDatabase?retryWrites=true&w=majority&appName=namasteNode")
 
 }
+
 
 module.exports = connectDB
 
